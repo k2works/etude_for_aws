ab7db24f645f95996509978e211dc933abec35d8
k2works <kakimomokuri@gmail.com>
2017-04-27 17:23:21 +0900

    リファクタリング
    ・テンプレートパターンを適用して実行クラスを具体的にする。

--- lib/etude_for_aws/vpc/vpc.rb
+++ lib/etude_for_aws/vpc/vpc.rb
@@ -1,51 +1,65 @@
 require 'aws-sdk'
 require 'dotenv'
-require 'json'
 
 class Vpc
-  def self.create
-    config = Configuration.new
-    cfm = Aws::CloudFormation::Client.new
-    stack_names = []
-    cfm.describe_stacks.stacks.each do |stack|
-      stack_names << stack.stack_name
-    end
-    unless stack_names.include?(config.stack_name)
-      resp = cfm.create_stack({
-                                  stack_name: config.stack_name,
-                                  template_body: config.template,
+  attr_reader :config
+
+  def initialize
+    @config = Configuration.new
+    @cfm = Aws::CloudFormation::Client.new
+  end
+
+  def create
+    stack_names = collect_stack_name
+    unless stack_names.include?(@config.stack_name)
+      resp = @cfm.create_stack({
+                                  stack_name: @config.stack_name,
+                                  template_body: @config.template,
                                   tags: [
                                       {
                                           key: 'Name',
-                                          value: config.stack_tag_value,
+                                          value: @config.stack_tag_value,
                                       },
                                   ],
                               })
-      config.stack_id = resp.stack_id
+      @config.stack_id = resp.stack_id
     end
-    config
   end
 
-  def self.destroy
-    config = Configuration.new
-    cfm = Aws::CloudFormation::Client.new
+  def destroy
+    stack_names = collect_stack_name
+    if stack_names.include?(@config.stack_name)
+      resp = @cfm.delete_stack({
+                                  stack_name: @config.stack_name,
+                              })
+      @config.stack_id = resp.to_s
+    end
+  end
+
+  private
+  def collect_stack_name
     stack_names = []
-    cfm.describe_stacks.stacks.each do |stack|
+    @cfm.describe_stacks.stacks.each do |stack|
       stack_names << stack.stack_name
     end
-    if stack_names.include?(config.stack_name)
-      resp = cfm.delete_stack({
-                                  stack_name: config.stack_name,
-                              })
-      config.stack_id = resp.to_s
-    end
-    config
+    stack_names
+  end
+end
+
+class OneAzOnePublicSubnetVpc < Vpc
+  def initialize
+    super
+    @config.stack_name = 'VPCtestStack'
+    template_path = '/lib/etude_for_aws/vpc/cfm_templates/'
+    template_file = 'vpc-1az-1subnet-pub.template'
+    file = Dir.pwd + template_path + template_file
+    @config.template = File.read(file)
+    @config.stack_tag_value = 'test'
   end
 end
 
 class Configuration
-  attr_reader :stack_name,:file,:template,:stack_tag_value
-  attr_accessor :stack_id
+  attr_accessor :stack_name,:template,:stack_tag_value,:stack_id
 
   def initialize
     Dotenv.load
@@ -54,11 +68,5 @@ class Configuration
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         region: ENV['AWS_DEFAULT_REGION']
     )
-    @stack_name = 'VPCtestStack'
-    template_path = '/lib/etude_for_aws/vpc/cfm_templates/'
-    template_file = 'vpc-1az-1subnet-pub.template'
-    @file = Dir.pwd + template_path + template_file
-    @template = File.read(file)
-    @stack_tag_value = 'test'
   end
 end
--- spec/vpc_spec.rb
+++ spec/vpc_spec.rb
@@ -1,17 +1,21 @@
 require "spec_helper"
 
 RSpec.describe Vpc do
-  describe '.create' do
-    it "create vpc using cloud formation" do
-      config = Vpc.create
-      expect(config.stack_id).not_to be_nil
+  context 'One Availability Zones One PublicSubnet Virtual private cloud' do
+    describe '.create' do
+      it "create vpc using cloud formation" do
+        vpc = OneAzOnePublicSubnetVpc.new
+        vpc.create
+        expect(vpc.config.stack_id).not_to be_nil
+      end
     end
-  end
 
-  describe '.destroy' do
-    it "destroy vpc using cloud formation" do
-      config = Vpc.destroy
-      expect(config.stack_id).to eq('Aws::EmptyStructure')
+    describe '.destroy' do
+      it "destroy vpc using cloud formation" do
+        vpc = OneAzOnePublicSubnetVpc.new
+        vpc.destroy
+        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+      end
     end
   end
 end
