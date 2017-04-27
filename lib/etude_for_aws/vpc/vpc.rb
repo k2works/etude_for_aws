6158b667401fc102f420887015b758bf1ff10304
k2works <kakimomokuri@gmail.com>
2017-04-27 16:42:14 +0900

    リファクタリング
    ・設定クラスを抽出する。

--- lib/etude_for_aws/vpc/vpc.rb
+++ lib/etude_for_aws/vpc/vpc.rb
@@ -4,51 +4,61 @@ require 'json'
 
 class Vpc
   def self.create
-    Dotenv.load
-    Aws.config.update(
-        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_DEFAULT_REGION']
-    )
-    file = Dir.pwd + '/lib/etude_for_aws/vpc/cfm_templates/vpc-1az-1subnet-pub.template'
-    template = File.read(file)
+    config = Configuration.new
     cfm = Aws::CloudFormation::Client.new
     stack_names = []
     cfm.describe_stacks.stacks.each do |stack|
       stack_names << stack.stack_name
     end
-    if stack_names.include?('VPCtestStack') == false
+    unless stack_names.include?(config.stack_name)
       resp = cfm.create_stack({
-                           stack_name: 'VPCtestStack',
-                           template_body: template,
-                           tags:[
-                               {
-                                   key:'Name',
-                                   value:'test',
-                               },
-                           ],
-                       })
-      return resp.to_s
+                                  stack_name: config.stack_name,
+                                  template_body: config.template,
+                                  tags: [
+                                      {
+                                          key: 'Name',
+                                          value: config.stack_tag_value,
+                                      },
+                                  ],
+                              })
+      config.stack_id = resp.stack_id
     end
+    config
   end
 
   def self.destroy
+    config = Configuration.new
+    cfm = Aws::CloudFormation::Client.new
+    stack_names = []
+    cfm.describe_stacks.stacks.each do |stack|
+      stack_names << stack.stack_name
+    end
+    if stack_names.include?(config.stack_name)
+      resp = cfm.delete_stack({
+                                  stack_name: config.stack_name,
+                              })
+      config.stack_id = resp.to_s
+    end
+    config
+  end
+end
+
+class Configuration
+  attr_reader :stack_name,:file,:template,:stack_tag_value
+  attr_accessor :stack_id
+
+  def initialize
     Dotenv.load
     Aws.config.update(
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         region: ENV['AWS_DEFAULT_REGION']
     )
-    cfm = Aws::CloudFormation::Client.new
-    stack_names = []
-    cfm.describe_stacks.stacks.each do |stack|
-      stack_names << stack.stack_name
-    end
-    if stack_names.include?('VPCtestStack') == true
-      resp = cfm.delete_stack({
-                                  stack_name: 'VPCtestStack',
-                              })
-      return resp.to_s
-    end
+    @stack_name = 'VPCtestStack'
+    template_path = '/lib/etude_for_aws/vpc/cfm_templates/'
+    template_file = 'vpc-1az-1subnet-pub.template'
+    @file = Dir.pwd + template_path + template_file
+    @template = File.read(file)
+    @stack_tag_value = 'test'
   end
 end
--- spec/vpc_spec.rb
+++ spec/vpc_spec.rb
@@ -3,13 +3,15 @@ require "spec_helper"
 RSpec.describe Vpc do
   describe '.create' do
     it "create vpc using cloud formation" do
-      expect(Vpc.create).to include('arn:aws:cloudformation:')
+      config = Vpc.create
+      expect(config.stack_id).not_to be_nil
     end
   end
 
   describe '.destroy' do
     it "destroy vpc using cloud formation" do
-      expect(Vpc.destroy).to eq('Aws::EmptyStructure')
+      config = Vpc.destroy
+      expect(config.stack_id).to eq('Aws::EmptyStructure')
     end
   end
 end
