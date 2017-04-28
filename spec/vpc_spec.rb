633cd44394e6a1b8a610342496ff42d388d55267
k2works <kakimomokuri@gmail.com>
2017-04-28 12:27:16 +0900

    テスト更新 #1
    現在テストを実施するとAWS上で実行してしまうので必要なときだけ実行するフラグを設定する。

--- spec/vpc_spec.rb
+++ spec/vpc_spec.rb
@@ -1,27 +1,28 @@
 require "spec_helper"
 
 RSpec.describe Vpc do
+  EXECUTE = true
+
   context 'One Availability Zones One PublicSubnet Virtual private cloud' do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(1)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#set_vpc_id' do
       it 'has vpc id' do
-        vpc.set_vpc_id
-        expect(vpc.config.vpc_id).not_to be_nil
+        if EXECUTE
+          vpc.set_vpc_id
+          expect(vpc.config.vpc_id).not_to be_nil
+        end
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
-        expect(vpc.config.vpc_id).to be_nil
+        check_destroy
       end
     end
   end
@@ -30,15 +31,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(2)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -47,15 +46,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(3)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -64,15 +61,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(4)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -81,15 +76,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(5)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -98,15 +91,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(6)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -115,15 +106,13 @@ RSpec.describe Vpc do
     let(:vpc) { create_vpc_instance(Vpc::TYPE.fetch(7)) }
     describe '#create' do
       it 'create vpc using cloud formation' do
-        vpc.create
-        expect(vpc.config.stack_id).not_to be_nil
+        check_create
       end
     end
 
     describe '#destroy' do
       it 'destroy vpc using cloud formation' do
-        vpc.destroy
-        expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+        check_destroy
       end
     end
   end
@@ -144,25 +133,40 @@ RSpec.describe Vpc do
       end
     end
   end
-end
 
-def create_vpc_instance(type)
-  case type
-    when :ONE_AZ_ONE_PUB
-      return OneAzOnePublicSubnetVpc.new
-    when :ONE_AZ_TWO_PUB
-      return OneAzTwoPublicSubnetVpc.new
-    when :ONE_AZ_ONE_PUB_PRI
-      return OneAzTwoPublicAndPrivateSubnetVpc.new
-    when :TWO_AZ_TWO_PRI
-      return TwoAzTwoPrivateSubnetVpc.new
-    when :TWO_AZ_TWO_PUB
-      return TwoAzTwoPublicSubnetVpc.new
-    when :TWO_AZ_ONE_PUB_RPI
-      return TwoAzOnePublicSubnetAndPrivateSubnetVpc.new
-    when :TWO_AZ_TWO_PUB_PRI
-      return TwoAzTwoPublicSubnetAndPrivateSubnetVpc.new
-    else
-      return NullVpc.new
+  def create_vpc_instance(type)
+    case type
+      when :ONE_AZ_ONE_PUB
+        return OneAzOnePublicSubnetVpc.new
+      when :ONE_AZ_TWO_PUB
+        return OneAzTwoPublicSubnetVpc.new
+      when :ONE_AZ_ONE_PUB_PRI
+        return OneAzTwoPublicAndPrivateSubnetVpc.new
+      when :TWO_AZ_TWO_PRI
+        return TwoAzTwoPrivateSubnetVpc.new
+      when :TWO_AZ_TWO_PUB
+        return TwoAzTwoPublicSubnetVpc.new
+      when :TWO_AZ_ONE_PUB_RPI
+        return TwoAzOnePublicSubnetAndPrivateSubnetVpc.new
+      when :TWO_AZ_TWO_PUB_PRI
+        return TwoAzTwoPublicSubnetAndPrivateSubnetVpc.new
+      else
+        return NullVpc.new
+    end
+  end
+
+  def check_create
+    if EXECUTE
+      vpc.create
+      expect(vpc.config.stack_id).not_to be_nil
+    end
+  end
+
+  def check_destroy
+    if EXECUTE
+      vpc.destroy
+      expect(vpc.config.stack_id).to eq('Aws::EmptyStructure')
+      expect(vpc.config.vpc_id).to be_nil
+    end
   end
 end
