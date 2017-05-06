32bb80d9f1f5469ada7a67be8592354981507f91
k2works <kakimomokuri@gmail.com>
2017-05-06 18:02:50 +0900

    リファクタリング #6
    ・テンプレートメソッドパターンを適用する

--- lib/etude_for_aws/vpc/vpc.rb
+++ lib/etude_for_aws/vpc/vpc.rb
@@ -67,21 +67,6 @@ module VPC
     end
 
     def create_subnets
-      @config.public_subnets.each do |v|
-        subnet_cidr_block = v['CONFIG']['SUBNET_CIDR_BLOCK'].first
-        name = v['CONFIG']['SUBNET_TAGS'].first['NAME']['VALUE']
-        key = v['CONFIG']['SUBNET_TAGS'].first['NAME']['KEY']
-        subnets = @gateway.select_subnets_by_name(name)
-        if subnets.empty?
-          subnet = VPC::Subnet.new
-          subnet.create(self,subnet_cidr_block)
-          resources = [subnet.subnet_id]
-          vpc_subnet_name_tag = {key: key, value: name}
-          tags = [vpc_subnet_name_tag,@config.vpc_group_tag]
-          @gateway.create_tags(resources,tags)
-          @public_subnets << subnet
-        end
-      end
     end
 
     def delete_subnets
@@ -89,6 +74,8 @@ module VPC
         subnet.delete(self)
       end
       @subnets = []
+      @private_subnets = []
+      @public_subnets = []
     end
 
     def create_internet_gateway
@@ -111,26 +98,6 @@ module VPC
     end
 
     def create_route_table
-      @public_subnets.each do |subnet|
-        @config.public_route_tables.each do |v|
-          destination_cidr_block = v['CONFIG']['DESTINATION_CIDR_BLOCK'].first
-          name = v['CONFIG']['ROUTE_TABLE_TAGS'].first['NAME']['VALUE']
-          key = v['CONFIG']['ROUTE_TABLE_TAGS'].first['NAME']['KEY']
-          route_tables = @gateway.select_route_tables_by_name(name)
-          if route_tables.empty?
-            route_table = VPC::RouteTable.new
-            route_table.create(self)
-            route_table.create_public_route(self,destination_cidr_block,@internet_gateway.internet_gateway_id)
-            route_table.associate_route_table(self,route_table.route_table_id,subnet.subnet_id)
-
-            resources = [route_table.route_table_id]
-            vpc_subnet_name_tag = {key: key, value: name}
-            tags = [vpc_subnet_name_tag,@config.vpc_group_tag]
-            @gateway.create_tags(resources,tags)
-            @public_route_tables << route_table
-          end
-        end
-      end
     end
 
     def delete_route_tables
@@ -138,8 +105,9 @@ module VPC
         route_table.delete(self)
       end
       @route_tables = []
+      @private_route_tables = []
+      @public_route_tables = []
     end
-
   end
 
   class VpcStub < Vpc
