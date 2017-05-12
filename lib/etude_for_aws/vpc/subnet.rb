d9769c20e5d08007a970031d1e1b6fb72e2d8e91
k2works <kakimomokuri@gmail.com>
2017-05-12 12:51:40 +0900

    リファクタリング
    ・使っていないメソッドを削除する

--- lib/etude_for_aws/vpc/subnet.rb
+++ lib/etude_for_aws/vpc/subnet.rb
@@ -7,14 +7,6 @@ module VPC
       @subnet_id = vpc.gateway.create_subnet(subnet_cidr_block,vpc.vpc_id,az)
     end
 
-    def create_private(vpc)
-      @subnet_id = vpc.gateway.create_subnet(vpc.config.subnet_cidr_block_private,vpc.vpc_id,vpc.config.vpc_name)
-    end
-
-    def create_public(vpc)
-      @subnet_id = vpc.gateway.create_subnet(vpc.config.subnet_cidr_block_public,vpc.vpc_id,vpc.config.vpc_name)
-    end
-
     def delete(vpc)
       vpc.gateway.delete_subnet(@subnet_id)
     end
